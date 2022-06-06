67b94bd5d139c382ac80207ed0f6d45adaaa1434
bachnqb100 <nguyenquybachqm01@gmail.com>
2022-06-06 16:35:49 +0700

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'hế nhô chị iu<3😚',
+    introDesc: `gửi này cho chị iu lèeeeeeeee`,
+    btnIntro: 'lẹt gô ehehe🤔🤔🤔',
+    title: 'iu chị ehehe 🥰🥰🥰 <br> giữ 80 năm nữa nhó😗😗😗',
+    desc: 'đang bám đừng chạy nhớ chị iu ới ehehe ',
+    btnYes: 'iuuu ơi là iuuu <33',
+    btnNo: 'khum bấm dc dou :333',
+    question:'đố biết sao bám chị iu thé 🤔🤔🤔',
+    btnReply: 'Gửi cho iem lè 😗',
+    reply: 'iu khum cần lý do dou ehehe:")))',
+    mess: 'cho iem vay 3k 80 năm nữa trả nhó ehehe 😘😘',
+    messDesc: 'giờ có chi bảo iem khum lè ehehe',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/b100.bnq' 
+}
